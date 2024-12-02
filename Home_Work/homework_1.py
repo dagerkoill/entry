266011183fbe941e35cdb4c46901662a3b61e8df
dagerkoill <denisdenisresetnakov@gmail.com>
2024-12-02 21:36:03 +0200

    Home_work_1.py
    Co-authored-by: leonidig <leonidig@users.noreply.github.com>
    Co-authored-by: xXx_Yura the god and destroyer_xXx <Yuriy202424@users.noreply.github.com>

---
+++ Home_Work/homework_1.py
@@ -0,0 +1,22 @@
+# a = int(input('Введите число 1 '))
+# b = int(input('Введите число 2 '))
+
+# print(a - b , a + b)
+
+
+# a = input('Введите ваш ник: ')
+# b = ('https://t.me/')
+
+# print(b + a)
+
+
+# a =input('Введите ваш никнейм: ') 
+# b =(f'https://t.me/{a}' ) 
+
+# print(b)
+
+
+# a =input('Введите ваше имя: ')
+# b = ('https://t.me/' + a)
+
+# print(b)
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('bebra')
+#   a = int(input("Введите число 1 "))
+#   b = int(input("Введите число 2 "))
+
+# print(a - b)
+
+
+
+# a = input("Введите имя: ")
+# b = a 
+
+# print(b)
+
+
+
+# a = 5
+# print(a)
+# 5
+# a + 1
+# print(a)
+# 5
+# a += 1 
+# print(a)
+# 6 
