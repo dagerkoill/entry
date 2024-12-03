91fdd2217d6862ab3afb6f03603a9a6b80cc9476
dagerkoill <denisdenisresetnakov@gmail.com>
2024-12-03 15:32:42 +0200

    conditional operators data types operators and expressions
    Co-authored-by: xXx_Yura the god and destroyer_xXx <Yuriy202424@users.noreply.github.com>
    Co-authored-by: leonidig <leonidig@users.noreply.github.com>

---
+++ Operators/operators.py
@@ -0,0 +1,76 @@
+#name = "Yura" #str string
+# age = 20 #int integer
+# price = 10.22 #float 
+# names = ["Yura","Denis","Andrey"] #list
+# customers = {"Cabbage" : 20, "Carrot" : 30, "Tomato" : 1488} #dict
+# friends = {"Leonidig", "Skoliozik", "IvanZolo"} #set
+# cars = ("mitsubisi", "foltswagen", "renovo logan") #tuple
+# is_gay = True # bool
+# is_sigma = False # bool
+# babushka = None 
+
+# print(type(name))
+
+# name = "Yura" #присваивание переменной значения Yura
+# name == "Yura" #проверка равно ли значение переменной Yura
+
+# if name == "Yura": 
+#     print("Hello Yura")
+# elif name == "Aboba":
+#     print("Idi nahui Aboba")
+# else:
+#     print("Ti ne Yura")
+
+# имеет один и тот же смысл из за того что нейм юра
+
+# if True:
+#     print("Hello Yura")
+# elif False:
+#     print("Idi nahui Aboba")
+# else:
+#     print("Ti ne Yura")
+
+# == если значения равны, <= если второе значение больше или равно первому, >= если первое значение больше или равно второму
+
+# age = int(input("Введите свой возраст: "))
+
+# if age <= 8 :
+#     print('Трахать можно')
+# elif age > 8 :
+#     print("Трахать нельзя")
+# else:
+#     print("Трахать нельзя")
+
+#name = 1
+
+# МОЖНО
+# sigmaabobusnuhaibebru = 1
+# __name__= sigmaabobusnuhaibebru
+# text = input(">")
+# name = "yura"
+# username = "yuraeblan"
+# city_name = "kalefornia"
+# _class = "some"
+
+
+# НЕЛЬЗЯ
+
+# #rgdfg = "aboba"
+# @a = 1
+# $sa = "bar"
+# class = "foo"
+# print = "abc"
+# abc = input(">")
+
+
+# answer = input("Ты в перерывах залипаешь по страшному? ")
+
+# if answer == "да":
+#     print("Ты чиловый паренек")
+# elif answer == "нет":
+#     print("стоит начать")
+# else:
+#     print("иди ты нахуй , сын шлюхи")
+
+
+# %= и //
---
+++ tst.py
@@ -0,0 +1,6 @@
+for i in range(11):
+    for j in range(11):
+        r = i ** j
+        print(r)
+        for y in range(r):
+            print(y ** y)
