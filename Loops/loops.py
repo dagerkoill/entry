76bd4b72c6749acce4d79d6d06f4f56feb0a86db
dagerkoill <denisdenisresetnakov@gmail.com>
2024-12-06 15:01:58 +0200

    Loops

---
+++ Loops/loops.py
@@ -0,0 +1,45 @@
+#цикл - это часть кода которая повторяет действие в зависимости от условий 
+
+
+
+#While Пока условие работает - выполняеться блок кода который находиться в While
+
+#For Сделать столько раз
+
+#num = 5
+
+# while num < 10:
+#     print("Hello")
+#     num += 1
+
+# for a in range(0,num):
+#     print("Hello")
+#     a += 1
+
+# 0 - False
+# 1 - True
+
+# name = "yura"
+
+# while name == "yura" : 
+#     print("Юра лучший учитель , ставлю лукас")
+#     name += "1"
+
+# for a in range(0 , 10):
+#     print(f"{a + 1}. Hello , world")
+
+# name = ""
+
+# while name != "rrrrr":
+#     print("пенис")
+#     name += "r"
+
+
+# a = input("Введите True или False: ")
+
+# if a.capitalize()== "True":
+#     for a in range(0 , 10):
+#         print("Hello")
+# elif a.capitalize()== "False":
+#     for a in range(0 , 5):
+#         print("bye")
--- tst.py
+++
@@ -1,6 +0,0 @@
-for i in range(11):
-    for j in range(11):
-        r = i ** j
-        print(r)
-        for y in range(r):
-            print(y ** y)
